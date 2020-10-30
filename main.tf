f37ce588f700470b1b6aae33fa4f0a75679a378a
tariq <kapooky101@gmail.com>
2020-10-29 20:26:01 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,128 @@
+provider "aws" {
+  region = "us-east-1"
+  #Hah, You thought I was gonna leave my credentials by mistake? Think Again!
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "production"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "producton"
+  }
+}
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+
+  }
+  tags = {
+    name = "prod"
+  }
+}
+
+
+resource "aws_subnet" "subnet-1" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "us-east-1a"
+  tags = {
+    name = "main"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+
+resource "aws_security_group" "web" {
+  name        = "Allow web traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HtTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_WEB"
+  }
+}
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id = aws_subnet.subnet-1.id
+  private_ips = [ "10.0.1.50"] #we are passing in a list of ip's in this field
+  security_groups = [aws_security_group.web.id]
+}
+
+resource "aws_eip" "one" {
+  vpc = true
+  network_interface = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
+resource "aws_instance" "my-first-server" {
+  ami = "ami-0817d428a6fb68645"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "lala"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+}
+  tags = {
+    name = "faggot"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c 'echo your very first webserver > /var/www/html/index.html'
+              EOF
+
+}
+
+output "serverip" {
+  value = aws_eip.one.public_ip
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+A very simple demo project just to get my hands dirty with Terraform.
+In this project, I design and deploy a apache webserver using terraform. 
+
+I followed this simple [tutorial](https://www.youtube.com/watch?v=SLB_c_ayRMo). Overall, It's not the most in depth tutorial about terraform but great for absolute beginners. 
